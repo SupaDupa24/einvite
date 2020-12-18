eb6989d29e7fd36e2b55f6f73024c5b8d765ff4d
SupaDupa24 <62140687+SupaDupa24@users.noreply.github.com>
2020-12-18 15:45:40 -0600

    tsparticles
    forgot to fix imports
    
    Co-Authored-By: Olive Brown Agency <hello@theolivebrownagency.com>

--- src/App.js
+++ src/App.js
@@ -7,39 +7,73 @@ import './App.css';
 
 
 const particlesOptions = {
-  "particles": {
-      "number": {
-          "value": 160,
-          "density": {
-              "enable": false
-          }
+    backgroundMode: {
+      enable: true,
+      zIndex: 0
+    },
+    background: {
+      color: "#000"
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: { enable: true, mode: "repulse" },
+        onHover: {
+          enable: true,
+          mode: "bubble",
+          parallax: { enable: false, force: 2, smooth: 10 }
+        },
+        resize: true
       },
-      "size": {
-          "value": 10,
-          "random": true
-      },
-      "move": {
-          "direction": "bottom",
-          "out_mode": "out"
-      },
-      "line_linked": {
-          "enable": false
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 0.3,
+          opacity: 1,
+          size: 4,
+          speed: 3
+        },
+        grab: { distance: 400, line_linked: { opacity: 0.5 } },
+        push: { particles_nb: 4 },
+        remove: { particles_nb: 2 },
+        repulse: { distance: 200, duration: 0.4 }
       }
-  },
-  "interactivity": {
-      "events": {
-          "onclick": {
-              "enable": true,
-              "mode": "remove"
-          }
+    },
+    particles: {
+      color: { value: "#fff" },
+      links: {
+        color: "#ffffff",
+        distance: 500,
+        enable: false,
+        opacity: 0.4,
+        width: 2
       },
-      "modes": {
-          "remove": {
-              "particles_nb": 10
-          }
+      move: {
+        attract: { enable: false, rotateX: 600, rotateY: 1200 },
+        direction: "bottom",
+        enable: true,
+        outMode: "out",
+        random: false,
+        size: true,
+        speed: 4,
+        straight: false
+      },
+      number: { density: { enable: true, area: 800 }, value: 400 },
+      opacity: {
+        random: true,
+        value: 0.5
+      },
+      shape: {
+        type: "circle"
+      },
+      size: {
+        random: true,
+        value: 10
       }
+    },
+    detectRetina: true
   }
-}
 
 const initialState = {
     name:'',
@@ -72,7 +106,7 @@ numberChange = (event) => {
       <div className='App'>
        <Particles
           className='particles'
-          params={{particlesOptions}} />
+          options={{particlesOptions}} />
         <Header />
         <Card />
         <Form />
